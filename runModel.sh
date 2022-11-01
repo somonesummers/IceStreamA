194bd4679963865559088a1acdd68ef85906b9fe
Paul Summers <psummers@stanford.edu>
2022-11-01 16:59:34 -0700

    better file names

--- runModel.sh
+++ runModel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --job-name=1Thm025
+#SBATCH --job-name=1Rise01
 #SBATCH --output=matlab_Main.“%j”.out
 #SBATCH --error=matlab_Main.“%j”.err
 #SBATCH --partition=serc
 #SBATCH --time=6:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
 #SBATCH --mail-type=ALL
 module load matlab
