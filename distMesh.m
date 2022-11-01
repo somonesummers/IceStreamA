c3ab6b8a66d4ed6bd294e70885e490664c352d7a
Paul Summers <psummers@stanford.edu>
2022-11-01 14:04:18 -0700

    blocking figures on linux, running distMesh with runGrid now

--- distMesh.m
+++ distMesh.m
@@ -22,21 +22,27 @@ ymin =  min(ybox);
 
 pv = [xbox; ybox]';
 
-figure(1)
+if(ismac)
+	figure(1)
+end
 
 [xy,t]=distmesh2d(@dpoly,@huniform,.05,[xmin,ymin;xmax,ymax]/1e5,pv/1e5,pv/1e5);
 xy = xy*1e5;
+disp("inital mesh complete")
+
 [u,v] = measures_interp('velocity',xy(:,1),xy(:,2));
 
 [xx,yy] = meshgrid(xmin:1e3:xmax,ymin:1e3:ymax);
 spd = measures_interp('speed',xx,yy);
-figure
-surf(xx,yy,zeros(size(spd)),log10(spd),'edgecolor','none')
-hold on 
-view(2)
-plot(pv(:,1),pv(:,2),'r-')
-axis equal
 
+if(ismac)
+    figure
+    surf(xx,yy,zeros(size(spd)),log10(spd),'edgecolor','none')
+    hold on 
+    view(2)
+    plot(pv(:,1),pv(:,2),'r-')
+    axis equal
+end
 
 %%
 if(sum(isnan(u)) + sum(isnan(v)) > 0)
@@ -47,12 +53,13 @@ if(sum(isnan(u)) + sum(isnan(v)) > 0)
     clear uFill vFill
 end
 
-figure
-trisurf(t,xy(:,1),xy(:,2),zeros(size(xy(:,1))),sqrt(u.^2+v.^2),...
-       'edgecolor','none')  
-colorbar
-view(2)
-
+if(ismac)
+	figure
+	trisurf(t,xy(:,1),xy(:,2),zeros(size(xy(:,1))),sqrt(u.^2+v.^2),...
+	       'edgecolor','none')  
+	colorbar
+	view(2)
+end
 % ep  = calcTrigridStrain(u,v,xy,3e3);
     dx =2e3;
     xi = min(xy(:,1))-dx:dx:max(xy(:,1))+dx;
@@ -87,10 +94,12 @@ view(2)
 
 
 fun =@(x,y) min(subplus(log(ep(x*1e5,y*1e5))+9),5);
-figure
-scatter(xy(:,1),xy(:,2),[],fun(xy(:,1)/1e5,xy(:,2)/1e5),'filled')
-colorbar
 
+if(ismac)
+	figure
+	scatter(xy(:,1),xy(:,2),[],fun(xy(:,1)/1e5,xy(:,2)/1e5),'filled')
+	colorbar
+end
 
 % figure(2)
 % scatter(xx(:),yy(:),[],lgSpd(:),'filled')
@@ -101,25 +110,30 @@ fd=@(p) dpoly(p,pv/1e5);
 % fh=@(p) 10-log(measures_interp('speed',p(:,1)*1e5,p(:,2)*1e5)+1);
 fh=@(p) ones(size(p,1),1) - fun(p(:,1),p(:,2))/6;
 
-figure
-edgeLength = .01;
+if(ismac)
+	figure
+end
+edgeLength = .005;
 [xy,t]=distmesh2d(fd,fh,edgeLength,[xmin,ymin;xmax,ymax]/1e5,pv/1e5);
-
+disp("Successfully meshed at " + edgeLength);
 xy = xy*1e5;
 se_bound = (ybox(4)-ybox(3))/(xbox(4)-xbox(3))*xy(:,1) - xy(:,2)  > (ybox(4)-ybox(3))/(xbox(4)-xbox(3))*xbox(3) - ybox(3)-dx/3;
 ne_bound = (ybox(3)-ybox(2))/(xbox(3)-xbox(2))*xy(:,1) - xy(:,2)  < (ybox(3)-ybox(2))/(xbox(3)-xbox(2))*xbox(2) - ybox(2)+dx/3;
 nw_bound = (ybox(2)-ybox(1))/(xbox(2)-xbox(1))*xy(:,1) - xy(:,2)  < (ybox(2)-ybox(1))/(xbox(2)-xbox(1))*xbox(1) - ybox(1)+dx/3;
 sw_bound = (ybox(1)-ybox(4))/(xbox(1)-xbox(4))*xy(:,1) - xy(:,2)  > (ybox(1)-ybox(4))/(xbox(1)-xbox(4))*xbox(4) - ybox(4)-dx/3;
 
-figure
-clear u
-u = measures_interp('speed',xy(:,1),xy(:,2));
-uFill = scatteredInterpolant(xy(~isnan(u),1),xy(~isnan(u),2),u(~isnan(u)));
-u(isnan(u)) = uFill(xy(isnan(u),1),xy(isnan(u),1));
-   
-trisurf(t,xy(:,1),xy(:,2),zeros(size(xy(:,1))),u,...
-       'edgecolor','none')   
-view(2)
-colorbar   
+if(ismac)
+	figure
+	clear u
+	u = measures_interp('speed',xy(:,1),xy(:,2));
+	uFill = scatteredInterpolant(xy(~isnan(u),1),xy(~isnan(u),2),u(~isnan(u)));
+	u(isnan(u)) = uFill(xy(isnan(u),1),xy(isnan(u),1));
+	   
+	trisurf(t,xy(:,1),xy(:,2),zeros(size(xy(:,1))),u,...
+	       'edgecolor','none')   
+	view(2)
+	colorbar   
+end
 
 save("gridRefinedRise" + strrep(string(edgeLength),"0.","") + ".mat");
+disp("Successfully Saved")
--- runGrid.sh
+++ runGrid.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-#SBATCH --job-name=IceStrA
-#SBATCH --output=matlab_test.“%j”.out
-#SBATCH --error=matlab_test.“%j”.err
+#SBATCH --job-name=Grid005
+#SBATCH --output=matlab_grid.“%j”.out
+#SBATCH --error=matlab_grid.“%j”.err
 #SBATCH --partition=serc
 #SBATCH --time=00:45:00
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
 #SBATCH --mail-type=ALL
 module load matlab
-matlab -nodisplay < lib/gridSandbox.m
+matlab -nodisplay < distMesh.m
