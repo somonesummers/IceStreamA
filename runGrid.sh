120729211a1246d8c098c5ccad82b0cfc9eadae9
Paul Summers <psummers@stanford.edu>
2023-04-10 15:55:00 -0700

    making larger mesh

--- runGrid.sh
+++ runGrid.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=matlab_grid.“%j”.out
 #SBATCH --error=matlab_grid.“%j”.err
 #SBATCH --partition=serc
-#SBATCH --time=00:40:00
+#SBATCH --time=02:40:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
 #SBATCH --mail-type=ALL
