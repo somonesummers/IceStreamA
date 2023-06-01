d10f46dc8b465d49093408a6363ed8e57321ab39
Paul Summers <psummers@stanford.edu>
2023-06-01 13:57:22 -0700

    handle bad file names

--- trimData.m
+++ trimData.m
@@ -8,28 +8,31 @@ cd ..
 for i = 1:numel(directoryNames)
     filename = append(currDir,'/',directoryNames{i});
 %     disp(filename)
-   
-    data = load(filename);
-    fix = 0;
-    try
-        fields = {'bed','bed_interp','surface','surf_interp','thick','thick_interp'};
-        data = rmfield(data,fields);
-        fix = 1 + 1;
-    catch
-        % no action if Goll fields already lacking
-    end
-    try
-        fields = {'dhdt_neg','dhdt_pos','thick_neg','thick_pos','thick_interp'};
-        data = rmfield(data,fields);
-        fix = fix + 1;
-    catch
-        % no action if DhDt fields already lacking
-    end
-    
-    if(fix == 1)
-        save(filename,'-struct','data');
-        disp("Succesfully reduced " + filename + " fix = " + fix)
-    else
-        disp("No changes made for " + filename + " fix = " + fix)
-    end
+   try
+        data = load(filename);
+        fix = 0;
+        try
+            fields = {'bed','bed_interp','surface','surf_interp','thick','thick_interp'};
+            data = rmfield(data,fields);
+            fix = 1 + 1;
+        catch
+            % no action if Goll fields already lacking
+        end
+        try
+            fields = {'dhdt_neg','dhdt_pos','thick_neg','thick_pos','thick_interp'};
+            data = rmfield(data,fields);
+            fix = fix + 1;
+        catch
+            % no action if DhDt fields already lacking
+        end
+
+        if(fix == 1)
+            save(filename,'-struct','data');
+            disp("Succesfully reduced " + filename + " fix = " + fix)
+        else
+            disp("No changes made for " + filename + " fix = " + fix)
+        end
+   catch
+       disp("Couldn't load " + filename + ". Skipping")
+   end
 end
